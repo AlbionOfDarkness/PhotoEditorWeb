9c453820906dacaa07dc84044e07920628bd9c77
AlbionOfDarkness <JaretCr@yandex.ru>
2025-11-30 17:31:43 +0400

    Test + struct

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'Hello, world!'
+
+@app.route('/about')
+def about():
+  return 'This is the about page'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+  return f'User {username}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
